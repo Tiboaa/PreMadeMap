9b5c9951347f40b3ed662f83631fa04aface99c6
Tiboaa <tibi200379@gmail.com>
2026-04-11 22:27:04 +0200

    Player kinda works

--- Scripts/test_map.gd
+++ Scripts/test_map.gd
@@ -10,7 +10,9 @@ var map_data = []
 var player_pos
 var player_clicked: bool = false
 var max_movement_points: int = 10
-var movement_ponts: int = max_movement_points
+var movement_points: int = max_movement_points
+var preview_route: Array = []
+var max_preview_distance: Vector2i = Vector2i(-1, -1)
 
 func _ready():
 	print("Map size:", map_size)
@@ -29,7 +31,15 @@ func _input(event):
 			Map.set_cell(1, prev_map_tile, 1, Vector2i(-1, -1))
 		prev_map_tile = current_map_tile
 			
-	
+	if event is InputEventMouseMotion and player_clicked:
+		var current_map_tile: Vector2i = Map.local_to_map(get_global_mouse_position())
+
+		if current_map_tile != Map.local_to_map(Player.global_position):
+			var route = get_preview_path(Map.local_to_map(Player.global_position), current_map_tile)
+
+			if route.size() > 0:
+				draw_preview(route)
+
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 		var click_pos: Vector2 = get_global_mouse_position()
 		var map_tile_clicked: Vector2i = Map.local_to_map(click_pos)
@@ -43,17 +53,15 @@ func _input(event):
 				player_clicked = false
 				Map.set_cell(1, map_tile_clicked, 1, Vector2i(-1, -1))
 		elif player_clicked == true and Map.local_to_map(Player.global_position) != map_tile_clicked:
-			#print("Mouse Click at: ", click_pos)
-			#print("Clicked Tile:", map_tile_clicked)
 			Map.set_cell(1, #Find or create a func to get_layer_id() reverse of get_layer_name()
 				map_tile_clicked, 1, Vector2i(5, 6))
 			Map.set_cell(1,Map.local_to_map(Player.global_position),1,Vector2i(-1,-1))
-			#print(map_tile_clicked)
-			#print(Map.local_to_map(Player.global_position))			
+
 			await get_tree().create_timer(0.5).timeout
 			Map.set_cell(1, map_tile_clicked, 1, Vector2i(-1, -1))
 			player_clicked = false
-			move_there(Map.local_to_map(Player.global_position), map_tile_clicked)
+			await move_there(Map.local_to_map(Player.global_position), map_tile_clicked)
+			clear_route()
 			#tile_clicked(Map.map_to_local(map_tile_clicked))
 
 func tile_clicked(map_tile):
@@ -199,21 +207,21 @@ func move_there(start_tile: Vector2i, destination: Vector2i):
 			print("Route:", route)
 			for i in range(1, route.size()):
 				var tile = route[i]
-				if movement_ponts >= map_data[tile.x][tile.y][5]:
+				if movement_points >= map_data[tile.x][tile.y][5]:
 					await tile_clicked(Map.map_to_local(tile))
 					total_cost += map_data[tile.x][tile.y][5]
 					print("I moved there: ", tile, " the cost is: ", map_data[tile.x][tile.y][5], " becouse the feature is: ", map_data[tile.x][tile.y][3])
 				else:
-					print("out of movement points, remaining movement points: ", movement_ponts)
+					print("out of movement points, remaining movement points: ", movement_points)
 					print("the movement cost would have been: ", map_data[tile.x][tile.y][5])
 					print("Total movement cost:", total_cost)
-					movement_ponts = max_movement_points
+					movement_points = max_movement_points
 					return
-				movement_ponts -= map_data[tile.x][tile.y][5]
-				print("remaining movement_points: ", movement_ponts)
+				movement_points -= map_data[tile.x][tile.y][5]
+				print("remaining movement_points: ", movement_points)
 				
 			print("Total movement cost:", total_cost)
-			movement_ponts = max_movement_points
+			movement_points = max_movement_points
 			return
 
 		open_set.erase(current)
@@ -237,7 +245,6 @@ func move_there(start_tile: Vector2i, destination: Vector2i):
 
 	print("No path found")
 
-
 func heuristic(a: Vector2i, b: Vector2i) -> int:
 	# Manhattan distance
 	return abs(a.x - b.x) + abs(a.y - b.y)
@@ -254,7 +261,6 @@ func reconstruct_path(came_from: Dictionary, current: Vector2i) -> Array:
 
 func in_a_star_array(current_tile: Array):
 	pass
-	
 
 func get_neighbors(tile: Vector2i):
 
@@ -271,3 +277,119 @@ func get_neighbors(tile: Vector2i):
 
 	return neighbors
 
+func get_preview_path(start_tile: Vector2i, destination: Vector2i) -> Array:
+	var open_set = [start_tile]
+	var came_from = {}
+
+	var g_score = {}
+	var f_score = {}
+
+	g_score[start_tile] = 0
+	f_score[start_tile] = heuristic(start_tile, destination)
+
+	while open_set.size() > 0:
+		var current = open_set[0]
+		for tile in open_set:
+			if f_score.get(tile, INF) < f_score.get(current, INF):
+				current = tile
+
+		if current == destination:
+			return reconstruct_path(came_from, current)
+
+		open_set.erase(current)
+
+		for neighbor in get_neighbors(current):
+			if neighbor.x < 0 or neighbor.y < 0 or neighbor.x >= map_size.x or neighbor.y >= map_size.y:
+				continue
+
+			if map_data[neighbor.x][neighbor.y][5] == 100:
+				continue
+
+			var tentative_g = g_score.get(current, INF) + map_data[neighbor.x][neighbor.y][5]
+
+			if tentative_g < g_score.get(neighbor, INF):
+				came_from[neighbor] = current
+				g_score[neighbor] = tentative_g
+				f_score[neighbor] = tentative_g + heuristic(neighbor, destination)
+
+				if neighbor not in open_set:
+					open_set.append(neighbor)
+
+	return []
+	
+func draw_preview(route: Array):
+	clear_preview()
+	if route.size() < 2:
+		return
+
+	var allowed_route: Array = []
+	var remaining = movement_points
+
+	allowed_route.append(route[0])
+
+	for i in range(1, route.size()):
+		var tile = route[i]
+		var cost = map_data[tile.x][tile.y][5]
+
+		if remaining < cost:
+			break
+		remaining -= cost
+		allowed_route.append(tile)
+
+	for i in range(1, allowed_route.size() - 1):
+		print("allowed route:", allowed_route)
+		var prev = allowed_route[i - 1]
+		var current = allowed_route[i]
+		var next = allowed_route[i + 1]
+
+		var atlas: Vector2i = get_direction_tile(prev, current, next)
+		Map.set_cell(3, current, 2, atlas)
+	
+	max_preview_distance = allowed_route[allowed_route.size()-1]
+	print("max dISTANCE: ", max_preview_distance)
+	Map.set_cell(1, max_preview_distance, 1, Vector2i(5, 6))
+	
+	for i in range(allowed_route.size(), route.size() - 1):
+		var prev = route[i - 1]
+		var current = route[i]
+		var next = route[i + 1]
+
+		var atlas: Vector2i = get_direction_tile(prev, current, next)
+		atlas = Vector2i(atlas.x + 2, atlas.y)
+		Map.set_cell(3, current, 2, atlas)
+
+	preview_route = route
+
+func clear_preview():
+	Map.set_cell(1, max_preview_distance, 1, Vector2i(-1, -1))
+	for tile in preview_route:
+		Map.set_cell(3, tile, 1, Vector2i(-1, -1))
+
+	preview_route.clear()
+
+func clear_route():
+	for tile in Map.get_used_cells(3):
+		Map.set_cell(3, tile, 1, Vector2i(-1, -1))
+
+func get_direction_tile(prev: Vector2i, current: Vector2i, next: Vector2i) -> Vector2i:
+	var from_dir_x = current.x - prev.x
+	var from_dir_y = current.y - prev.y
+
+	var to_dir_x = next.x - current.x
+	var to_dir_y = next.y - current.y
+	
+	if from_dir_x == 0 and to_dir_x == 0:
+		return Vector2i(1, 2)
+	if from_dir_y == 0 and to_dir_y == 0:
+		return Vector2i(0, 2)
+	# corners
+	if (from_dir_x == 1 and to_dir_y == 1) or (from_dir_y == -1 and to_dir_x == -1):
+		return Vector2i(1, 0)
+	if (from_dir_x == -1 and to_dir_y == 1) or (from_dir_y == -1 and to_dir_x == 1):
+		return Vector2i(0, 0) 
+	if (from_dir_x == 1 and to_dir_y == -1) or (from_dir_y == 1 and to_dir_x == -1):
+		return Vector2i(1, 1)
+	if (from_dir_x == -1 and to_dir_y == -1) or (from_dir_y == 1 and to_dir_x == 1):
+		return Vector2i(0, 1) 
+
+	return Vector2i(-1, -1)
